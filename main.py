7208035ba01a7d16c29ffbdea84681f71eb7521f
yerin <9yelin9@gmail.com>
2025-03-20 09:06:41 +0000

    Update pb2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 import pandas as pd
 
@@ -7,36 +8,44 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 
 from data import SimpleSeqDataset
-from model import ThreeLayerNet
+from model import ThreeLayerNetOneHot, ThreeLayerNetWordEmb
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--onehot', action='store_true')
+args = parser.parse_args()
 
 STUDENT_ID = "20242056"
 N_CLASS = 19
 MAX_LEN = 20
+EMB_DIM = 0 if args.onehot else 10
 
 if torch.cuda.is_available():
 	torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
-def train(train_loader, model, loss_fn, optimizer, scheduler, n_epoch=100, tol=1e-6):
+def train(train_loader, model, loss_fn, optimizer, scheduler, n_epoch=100, tol=1e-6, pat=3):
 	print(f'------ Train (n_epoch={n_epoch}, tol={tol}) ------')
-	print('%6s%14s%14s' % ('epoch', 'loss_epoch', 'delta'))
+	print('%6s%14s%14s' % ('epoch', 'loss', 'delta'))
 
-	loss_epoch_old, delta = 100, 100
+	loss_old, delta, cnt = 100, 100, 0
 	for epoch in range(n_epoch):
-		loss_epoch = 0
+		loss = 0
 		for x, y in train_loader:
 			optimizer.zero_grad()
 			y_pred = model(x)
-			loss = loss_fn(y_pred, y)
-			loss.backward()
-			loss_epoch += loss.item()
+			loss_batch = loss_fn(y_pred, y)
+			loss_batch.backward()
+			loss += loss_batch.item()
 			optimizer.step()
 		scheduler.step()
 
-		loss_epoch /= len(train_loader)
-		delta = abs(loss_epoch - loss_epoch_old)
-		print(f'{epoch:6d}{loss_epoch:14f}{delta:14f}')
-		if delta < tol: break
-		else: loss_epoch_old = loss_epoch
+		# Early stopping
+		loss /= len(train_loader)
+		delta = abs(loss - loss_old)
+		print(f'{epoch:6d}{loss:14f}{delta:14f}')
+		if delta < tol:
+			if cnt < pat: break
+			else: cnt += 1
+		else: loss_old = loss
 
 	print(f'--------------------------------------------', end='\n\n')
 
@@ -46,30 +55,34 @@ def test(test_loader, model, label_dict):
 
 	df = []
 	with torch.no_grad():
-		for i, (x, _) in enumerate(test_loader):
+		for i, (x, _) in enumerate(test_loader, start=1):
 			y_pred = model(x)
-			df.append([f'S{i+1:03d}', label_dict_inv[y_pred.argmax(dim=1).item()]])
+			df.append([f'S{i:03d}', label_dict_inv[y_pred.argmax(dim=1).item()]])
 	df = pd.DataFrame(df, columns=['id', 'pred'])
 	
 	return df
 
 def main():
 	print('Load Data...', end=' ')
-	train_dset = SimpleSeqDataset('./dataset/simple_seq.train.csv', max_len=MAX_LEN)
-	test_dset  = SimpleSeqDataset('./dataset/simple_seq.test.csv',  max_len=MAX_LEN, token_dict=train_dset.token_dict)
+	train_dset = SimpleSeqDataset('./dataset/simple_seq.train.csv', max_len=MAX_LEN, onehot=args.onehot)
+	test_dset  = SimpleSeqDataset('./dataset/simple_seq.test.csv',  max_len=MAX_LEN, onehot=args.onehot, token_dict=train_dset.token_dict)
 	print('Done', end='\n\n')
 
 	train_loader = DataLoader(train_dset, batch_size=16, shuffle=True,  drop_last=True)
 	test_loader  = DataLoader(test_dset,  batch_size=1,  shuffle=False, drop_last=False)
 
-	model = ThreeLayerNet(train_dset.data.shape[1], 1000, 100, N_CLASS)
+	if args.onehot:
+		model = ThreeLayerNetOneHot(MAX_LEN, len(train_dset.token_dict), 1000, 100, N_CLASS)
+	else:
+		model = ThreeLayerNetWordEmb(MAX_LEN, EMB_DIM, len(train_dset.token_dict), 1000, 100, N_CLASS)
+
 	loss_fn = nn.CrossEntropyLoss()
 	optimizer = optim.SGD(model.parameters(), lr=1e-1)
-	scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=1e-2)
+	scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
 
-	train(train_loader, model, loss_fn, optimizer, scheduler, n_epoch=100, tol=1e-6)
-	df1 = test(test_loader, model, train_dset.label_dict)
-	df1.to_csv(f'{STUDENT_ID}_simple_seq.p1.answer.csv', index=False)
+	train(train_loader, model, loss_fn, optimizer, scheduler, n_epoch=1000, tol=1e-4, pat=3)
+	df = test(test_loader, model, train_dset.label_dict)
+	df.to_csv(f'{STUDENT_ID}_simple_seq.p{2-args.onehot}.answer.csv', index=False)
 
 if __name__ == "__main__":
 	main()
--- model.py
+++ model.py
@@ -9,17 +9,35 @@ class Linear(nn.Module):
 
 		self.w = nn.Parameter(torch.randn(output_size, input_size))
 		self.b = nn.Parameter(torch.zeros(output_size))
-
 		nn.init.xavier_uniform_(self.w)
 
 	def forward(self, x):
 		return x @ self.w.T + self.b
 
-class ThreeLayerNet(nn.Module):
-	def __init__(self, input_size, hidden1_size, hidden2_size, output_size):
+class Embedding(nn.Module):
+	def __init__(self, token_size, emb_dim, pad_idx=0):
+		super().__init__()
+
+		self.w = nn.Parameter(torch.randn(token_size, emb_dim))
+		nn.init.xavier_uniform_(self.w)
+
+		# Padding
+		self.pad_idx = pad_idx
+		with torch.no_grad(): self.w[pad_idx] = 0
+		self.w.register_hook(self.padding)
+
+	def padding(self, grad):
+		grad[self.pad_idx] = 0
+		return grad
+
+	def forward(self, x):
+		return self.w[x]
+
+class ThreeLayerNetOneHot(nn.Module):
+	def __init__(self, max_len, token_size, hidden1_size, hidden2_size, output_size):
 		super().__init__()	
 
-		self.l1 = Linear(input_size,   hidden1_size)
+		self.l1 = Linear(max_len * token_size, hidden1_size)
 		self.l2 = Linear(hidden1_size, hidden2_size)
 		self.l3 = Linear(hidden2_size, output_size)
 
@@ -27,6 +45,33 @@ class ThreeLayerNet(nn.Module):
 		self.bn2 = nn.BatchNorm1d(hidden2_size)
 
 	def forward(self, x):
+		x = self.l1(x)
+		x = self.bn1(x)
+		x = torch.sigmoid(x)
+
+		x = self.l2(x)
+		x = self.bn2(x)
+		x = torch.sigmoid(x)
+
+		x = self.l3(x)
+		return x
+
+class ThreeLayerNetWordEmb(nn.Module):
+	def __init__(self, max_len, emb_dim, token_size, hidden1_size, hidden2_size, output_size):
+		super().__init__()	
+
+		self.emb = Embedding(token_size, emb_dim)
+			
+		self.l1 = Linear(max_len * emb_dim, hidden1_size)
+		self.l2 = Linear(hidden1_size, hidden2_size)
+		self.l3 = Linear(hidden2_size, output_size)
+
+		self.bn1 = nn.BatchNorm1d(hidden1_size)
+		self.bn2 = nn.BatchNorm1d(hidden2_size)
+
+	def forward(self, x):
+		x = self.emb(x).flatten(start_dim=1)
+
 		x = self.l1(x)
 		x = self.bn1(x)
 		x = torch.sigmoid(x)
