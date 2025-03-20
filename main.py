d2f5926202bef6cabcfd152dd69855dae49cf6ce
yerin <9yelin9@gmail.com>
2025-03-20 09:59:08 +0000

    Update pb2

--- main.py
+++ main.py
@@ -23,31 +23,38 @@ if torch.cuda.is_available():
 	torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
 def train(train_loader, model, loss_fn, optimizer, scheduler, n_epoch=100, tol=1e-6, pat=3):
-	print(f'------ Train (n_epoch={n_epoch}, tol={tol}) ------')
-	print('%6s%14s%14s' % ('epoch', 'loss', 'delta'))
+	print(f'---------- Train (n_epoch={n_epoch}, tol={tol}) ----------')
+	print('%6s%14s%14s%14s' % ('epoch', 'acc', 'loss', 'delta'))
 
 	loss_old, delta, cnt = 100, 100, 0
 	for epoch in range(n_epoch):
-		loss = 0
+		acc, loss = 0, 0
 		for x, y in train_loader:
 			optimizer.zero_grad()
+
 			y_pred = model(x)
+
+			acc_batch = (y_pred.argmax(dim=1) == y).float().mean()
 			loss_batch = loss_fn(y_pred, y)
 			loss_batch.backward()
+
+			acc += acc_batch.item()
 			loss += loss_batch.item()
+
 			optimizer.step()
 		scheduler.step()
 
 		# Early stopping
+		acc /= len(train_loader)
 		loss /= len(train_loader)
 		delta = abs(loss - loss_old)
-		print(f'{epoch:6d}{loss:14f}{delta:14f}')
+		print(f'{epoch:6d}{acc:14f}{loss:14f}{delta:14f}')
 		if delta < tol:
 			if cnt < pat: break
 			else: cnt += 1
 		else: loss_old = loss
 
-	print(f'--------------------------------------------', end='\n\n')
+	print(f'----------------------------------------------------', end='\n\n')
 
 def test(test_loader, model, label_dict):
 	label_dict_inv = {v:k for k, v in label_dict.items()}
@@ -77,10 +84,11 @@ def main():
 		model = ThreeLayerNetWordEmb(MAX_LEN, EMB_DIM, len(train_dset.token_dict), 1000, 100, N_CLASS)
 
 	loss_fn = nn.CrossEntropyLoss()
-	optimizer = optim.SGD(model.parameters(), lr=1e-1)
-	scheduler = optim.lr_scheduler.CyclicLR(optimizer, base_lr=1e-3, max_lr=1e-1)
+	#optimizer = optim.SGD(model.parameters(), lr=1e-1)
+	optimizer = optim.Adam(model.parameters(), lr=1e-2)
+	scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
 
-	train(train_loader, model, loss_fn, optimizer, scheduler, n_epoch=1000, tol=1e-6, pat=3)
+	train(train_loader, model, loss_fn, optimizer, scheduler, n_epoch=1000, tol=1e-5, pat=3)
 	df = test(test_loader, model, train_dset.label_dict)
 	df.to_csv(f'{STUDENT_ID}_simple_seq.p{2-args.onehot}.answer.csv', index=False)
 
